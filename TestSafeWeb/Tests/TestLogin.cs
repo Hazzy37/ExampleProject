57c2c4b6f7c7f3bf819a82e931c4492154302ff0
Steven Harrop <stevenharrop1@icloud.com>
2018-04-01 18:49:21 +0100

    Update to tests to push to Git hopefully

--- TestSafeWeb/Tests/TestLogin.cs
+++ TestSafeWeb/Tests/TestLogin.cs
@@ -4,11 +4,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TestSafeWeb
 {
     [TestClass]
-    public class UnitTest1
+    public class TestLogin
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestLogin01()
         {
+            Assert.IsTrue(true);
         }
     }
 }
